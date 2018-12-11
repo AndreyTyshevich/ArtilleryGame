3cb7e8146fbec29043d610ad2e0b1ab152344049
AndreyTyshevich <43672780+AndreyTyshevich@users.noreply.github.com>
2018-12-11 20:04:30 +0300

    Update shell.py

--- shell.py
+++ shell.py
@@ -1,2 +1,23 @@
 class Shell():
-    pass
+    def __init__(self, canv, x, y):
+        self.canvas = canv
+        self.x = x
+        self.y = y
+        self.r = 10
+        self.vx = 0
+        self.vy = 0
+        self.color = 'gray'
+        self.id = canv.create_oval(self.x-self.r, self.y-self.r, self.x+self.r, self.y+self.r, fill=self.color)
+        self.live = 100
+
+    def draw(self):
+        pass
+
+    def set_coords(self):
+        canv.coords(self.id, self.x - self.r, self.y - self.r, self.x + self.r, self.y + self.r)
+
+    def move(self):
+        pass
+
+    def hittest(self):
+        pass
