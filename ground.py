0dad32e0cd9fe92c0a596018e7ccb1e3b74fee57
AndreyTyshevich <43672780+AndreyTyshevich@users.noreply.github.com>
2018-12-11 20:03:56 +0300

    Update ground.py

--- ground.py
+++ ground.py
@@ -8,15 +8,17 @@ class Ground:
         self.coord = 0
 
     def draw(self):
-        h = random.randint(250, 650)
+        h = random.randint(300, 500)
+        d = 1
         for x in range(1200):
             self.coord = (x, h)
             self.coords.append(self.coord)
-            self.canvas.create_line(x, 700, x, h, width=7, fill='green')
-            if h > 670:
-                i = random.randint(-25, 0)
-            elif 100 <= h <= 670:
-                i = random.randint(-5, 5)
-            elif h < 80:
-                i = random.randint(0, 25)
+            self.canvas.create_line(x, 700, x, h, width=7, fill='ForestGreen')
+            if h > 500 or h < 300:
+                d = -d
+                i = d*random.randint(0, 1)
+            elif 380 < h < 420:
+                i = d*random.randint(0, 2)
+            elif 300 <= h <= 380 or 420 <= h <= 500:
+                i = d*random.randint(0, 1)
             h = h + i
