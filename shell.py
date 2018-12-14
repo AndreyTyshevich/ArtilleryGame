cacf3c6a79b5dac3a35a5eaa3e6593bf5ee88c37
AndreyTyshevich <43672780+AndreyTyshevich@users.noreply.github.com>
2018-12-14 15:12:58 +0300

    Update shell.py

--- shell.py
+++ shell.py
@@ -1,11 +1,11 @@
 class Shell():
-    def __init__(self, canv, k):
+    def __init__(self, canv, x, y, vx, vy, k):
         self.canvas = canv
-        self.x = 0
-        self.y = 0
+        self.x = x
+        self.y = y
         self.r = 10
-        self.vx = 0
-        self.vy = 0
+        self.vx = vx
+        self.vy = vy
         self.k = k
         self.color = 'gray'
         self.id = canv.create_oval(self.x-self.r, self.y-self.r, self.x+self.r, self.y+self.r, fill=self.color)
