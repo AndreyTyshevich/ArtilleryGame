53c0bd942ab4aa7b4d0bbda832435485567040f7
AndreyTyshevich <43672780+AndreyTyshevich@users.noreply.github.com>
2018-12-17 15:51:48 +0300

    Update flag.py
    Немного изменен принцип работы в реалиях переъода на физические координаты

--- flag.py
+++ flag.py
@@ -1,12 +1,12 @@
 class Flag:
-    def __init__(self, canv, x_peak, x, y, k):
-        self.x_peak = x_peak
-        self.y_peak = y - 220
+    def __init__(self, canv, x, y, k):
         self.x = x
-        self.y = y
+        self.y = 700 - y
         self.k = k
+        self.x_peak = x + self.k * 20
+        self.y_peak = self.y - 180
         self.canvas = canv
 
     def draw_flag(self):
-        self.canvas.create_line(self.x, self.y-250, self.x, self.y, fill='#654321', width=7)
-        self.canvas.create_polygon(self.x, self.y-250, self.x_peak, self.y_peak, self.x, self.y-190, fill='#7B917B')
+        self.canvas.create_line(self.x, self.y-200, self.x, self.y, fill='#654321', width=7)
+        self.canvas.create_polygon(self.x, self.y-160, self.x_peak, self.y_peak, self.x, self.y-200, fill='#7B917B')
