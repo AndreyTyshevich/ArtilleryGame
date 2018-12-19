89e1b15ae2e5ed41acedd068c984286491b4ba4a
StGalya <43673285+StGalya@users.noreply.github.com>
2018-12-19 23:51:12 +0300

    добавила картинки)

---
+++ game.py
@@ -0,0 +1,122 @@
+from ground import *
+from cannon import *
+from flag import *
+
+from tkinter import *
+from math import *
+from random import randrange as randint, choice
+import time
+from enum import Enum
+
+sleep_time = 35
+screen_width = 1200
+screen_height = 700
+shell_radius = 5
+dt = 10  # времени между кадрами обсчёта
+g = -9.8
+
+
+class GameState(Enum):
+    TANK_IS_AIMING = 1
+    SHELL_IS_FLYING = 2
+
+
+def screen(x, y):
+    return x, screen_height - y
+
+
+class Start_game():
+    def __init__(self, root):
+        global canvas
+        canvas = Canvas(root)
+        canvas["width"] = screen_width
+        canvas["height"] = screen_height
+        canvas.pack(fill=BOTH, expand=1)
+        self.cannons = []
+        '''photo = PhotoImage(file="123456.png")
+        canvas.create_image(100, 100, anchor=CENTER, image=photo)
+        Label(root, image=photo).place(x=600, y=200)
+        Background(canvas)'''
+
+        photo = PhotoImage(file="backgr.png")
+        canvas.create_image(600,350, anchor=CENTER, image=photo)
+        label = Label(image=photo)
+        label.image = photo
+
+        photo = PhotoImage(file="sun.png")
+        canvas.create_image(1000,100, anchor=CENTER, image=photo)
+        label = Label(image=photo)
+        label.image = photo
+
+        Background(canvas)
+        self.ground = Ground(canvas)
+        self.ground.draw()
+
+        photo = PhotoImage(file="123456.png")
+        canvas.create_image(800, 280, anchor=CENTER, image=photo)
+        label = Label(image=photo)
+        label.image = photo
+
+        x = randint(150, 1050)
+        y = self.ground.height[round(x)]
+        k = randint(-4, 4)
+        flag = Flag(canvas, x, y, k)
+        flag.draw_flag()
+
+        x = randint(50, 550)
+        y = self.ground.height[round(x)]
+        self.gamer1 = Cannon(x, y, canvas, k)
+        self.cannons.append(Cannon(x, y, canvas, k))
+
+        x = randint(750, 1150)
+        y = self.ground.height[round(x)]
+        self.gamer2 = Cannon(x, y, canvas, k)
+        self.cannons.append(Cannon(x, y, canvas, k))
+
+        self.current_player = 0
+        self.shells = []
+
+        canvas.bind("<Button-1>", self.mouse_click)
+        canvas.bind("<Motion>", self.mouse_motion)
+        # root.bind('<Key>', self.move)
+        self.game_state = GameState.TANK_IS_AIMING
+
+    def mouse_motion(self, event):
+        if self.game_state != GameState.TANK_IS_AIMING:
+            return
+        cannon = self.cannons[self.current_player]
+        cannon.target(event.x, screen_height - event.y)
+
+    def mouse_click(self, event):
+        if self.game_state != GameState.TANK_IS_AIMING:
+            return
+        cannon = self.cannons[self.current_player]
+        x, y = screen(event.x, event.y)
+        cannon.target(x, y)
+        shell = cannon.shoot(x, y)
+        self.shells.append(shell)
+
+        self.game_state = GameState.SHELL_IS_FLYING
+        canvas.after(sleep_time, self.shell_flying)
+
+        self.current_player = (self.current_player + 1) % 2
+
+    def shell_flying(self, *ignore):
+        if self.game_state != GameState.SHELL_IS_FLYING:
+            return
+        canvas.after(sleep_time, self.shell_flying)
+
+        for shell in self.shells:
+            shell.move()
+
+        for shell in self.shells:
+            if self.ground.check_collision(shell):
+                self.ground.explode(shell)
+                self.game_state = GameState.TANK_IS_AIMING
+        if self.game_state != GameState.SHELL_IS_FLYING:
+            self.shells.clear()
+
+
+root_window = Tk()
+window = Start_game(root_window)
+root_window.mainloop()
