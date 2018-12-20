8be1043077647dfbc993ad86e1bac13ba3d92454
AndreyTyshevich <43672780+AndreyTyshevich@users.noreply.github.com>
2018-12-20 22:10:24 +0300

    Update flag.py
    Теперь флаг может менять свою геометрию

--- flag.py
+++ flag.py
@@ -1,12 +1,15 @@
 class Flag:
-    def __init__(self, canv, x, y, k):
+    def __init__(self, canvas, x, y, k):
         self.x = x
         self.y = 700 - y
         self.k = k
-        self.x_peak = x + self.k * 20
+        self.x_peak = x + self.k * 10
         self.y_peak = self.y - 180
-        self.canvas = canv
+        self.canvas = canvas
+        self.stick = self.canvas.create_line(self.x, self.y-200, self.x, self.y, fill='#654321', width=7)
+        self.triangle = self.canvas.create_polygon(self.x, self.y-160, self.x_peak, self.y_peak, self.x, self.y-200, fill='#7B917B')
 
-    def draw_flag(self):
-        self.canvas.create_line(self.x, self.y-200, self.x, self.y, fill='#654321', width=7)
-        self.canvas.create_polygon(self.x, self.y-160, self.x_peak, self.y_peak, self.x, self.y-200, fill='#7B917B')
+    def change_power(self, k):
+        x_peak = self.x + k * 20
+        self.k = k
+        self.canvas.coords(self.triangle, self.x, self.y - 160, x_peak, self.y_peak, self.x, self.y - 200)
