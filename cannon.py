6c953718b86d7a87687f4883332c020b9020b80e
AndreyTyshevich <43672780+AndreyTyshevich@users.noreply.github.com>
2018-12-21 12:34:23 +0300

    Update cannon.py
    последние штрихи

--- cannon.py
+++ cannon.py
@@ -1,7 +1,8 @@
+from tkinter import messagebox
 screen_width = 1200
 screen_height = 700
 shell_radius = 10
-dt = 0.3
+dt = 0.2
 g = -9.8
 
 
@@ -13,11 +14,11 @@ class Shell:
     def __init__(self, x, y, r, Vx, Vy, canvas, k):
         self.color = 'gray'
         self.x, self.y, self.r = x, y, r
-        self.Vx, self.Vy = 5*Vx, 5*Vy
+        self.Vx, self.Vy = 3*Vx, 3*Vy
         self._canvas = canvas
         self.circle = canvas.create_oval(screen(x - r, y - r), screen(x + r, (y + r)), fill=self.color)
-        self.damage_radius = 40
-        self.damage = 10
+        self.damage_radius = 10
+        self.damage = 30
         self.k = k
 
     def move(self):  # Метод описывает характер изменения координат между отрисовками и перемещает снаряд
@@ -33,20 +34,22 @@ class Shell:
         self._canvas.coords(self.circle, x1, y1, x2, y2)
 
     def destroy(self):  # Метод убирает снаряд с холста
-        self._canvas.delete(self.circle)
+        try:
+            self._canvas.delete(self.circle)
+        except:
+            pass
 
 
 class Cannon:
     max_cannon_length = 30
 
-    def __init__(self, x, y, canvas):
+    def __init__(self, x, y, canvas, root):
         self._canvas = canvas
         self.x, self.y = x, y
-        # self.power = 10
-        # self.on = 0 дописать
         self.length_x = 0
         self.length_y = -30
         self.r = 20
+
         self.cannon = self._canvas.create_line(screen(self.x, self.y,), screen(self.x +
                                                                                self.length_x, self.y + self.length_y),
                                                width=7, fill='black', tag='cannon')
@@ -54,12 +57,24 @@ class Cannon:
                                                  screen(self.x+self.r, self.y+self.r), fill="black")
         self.health = 100
 
-        '''
-        canvas.create_text(100, 100, text="здоровье:")  # нужно изменять здоровье. для этого нужны попадания
-        canvas.create_text(150, 100, text=self.health)
-        canvas.create_text(900, 100, text="здоровье:")
-        canvas.create_text(950, 100, text=self.health)
-        '''
+        self._root = root
+        self.text_id = -500
+        self.current_shell = -500
+        self.money = 0
+
+        if 100 <= self.x <= 550:
+            self._canvas.create_text(100, 100, text="Прочность:")
+            self.text_id = self._canvas.create_text(150, 100, text=self.health)
+        else:
+            self._canvas.create_text(1050, 100, text="Прочность:")
+            self.text_id = self._canvas.create_text(1100, 100, text=self.health)
+
+        if 100 <= self.x <= 550:
+            self._canvas.create_text(100, 120, text="Очки:")
+            self.money_id = self._canvas.create_text(150, 120, text=self.money)
+        else:
+            self._canvas.create_text(1050, 120, text="Очки:")
+            self.money_id = self._canvas.create_text(1100, 120, text=self.money)
 
     def target(self, x, y):  # Метод описывает наведение
         self.length_x = (x - self.x)
@@ -74,29 +89,34 @@ class Cannon:
         self.cannon = self._canvas.create_line(x1, y1, x2, y2, width=7, fill='black', tag='cannon')
         self._canvas.coords(self.cannon, x1, y1, x2, y2)
 
-    # еще не дописана
-    def power_up(self):
-        if self.on:
-            if self.power < 100:
-                self.power += 1
-            self._canvas.itemconfig(self.cannon, fill='orange')
-        else:
-            self._canvas.itemconfig(self.cannon, fill='black')
-
     def shoot(self, x, y, k):  # Метод описывает выстрел
         self.target(x, y)
         Vx = self.length_x
         Vy = self.length_y
         return Shell(self.x + self.length_x, self.y + self.length_y, shell_radius, Vx, Vy, self._canvas, k)
 
+    def earn_points(self):
+        self.money += 10
+        self._canvas.itemconfig(self.money_id, text=self.money)
 
-'''
+    def spend_points(self):
+        self.money -= 20
+        self._canvas.itemconfig(self.money_id, text=self.money)
+
+    def armor_up(self):
+        self.health += 20
+        self._canvas.itemconfig(self.text_id, text=self.health)
+
     def take_damage(self, damage):
-        damage = 25
         self.health -= damage
+        self._canvas.itemconfig(self.text_id, text=self.health)
         if self.health <= 0:
-            self._canvas.create_text(100, 100, text="здоровье:")  # нужно изменять здоровье. для этого нужны попадания
-            self._canvas.create_text(150, 100, text=self.health)
-            self._canvas.create_text(900, 100, text="здоровье:")
-            self._canvas.create_text(950, 100, text=self.health)
-'''
+            self._canvas.delete("all")
+            self._canvas.destroy()
+            self._root.destroy()
+            infotext = " "
+            if 50 <= self.x <= 550:
+                infotext = "Second player wins"
+            else:
+                infotext = "First player wins"
+            messagebox.showinfo("Game Over", infotext)
