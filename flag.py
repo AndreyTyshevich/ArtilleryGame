4028abc0a2d548f4a5482d43b05028349ecabbcc
AndreyTyshevich <43672780+AndreyTyshevich@users.noreply.github.com>
2018-12-21 12:34:56 +0300

    Update flag.py
    последние штрихи

--- flag.py
+++ flag.py
@@ -3,14 +3,14 @@ class Flag:
         self.x = x
         self.y = 700 - y
         self.k = k
-        self.x_peak = x + self.k * 10
+        self.x_peak = x + self.k * 5
         self.y_peak = self.y - 180
         self.canvas = canvas
         self.stick = self.canvas.create_line(self.x, self.y-200, self.x, self.y, fill='#654321', width=7)
         self.triangle = self.canvas.create_polygon(self.x, self.y-160,
                                                    self.x_peak, self.y_peak, self.x, self.y-200, fill='#7B917B')
 
-    def change_power(self, k):  # Метод описывает изменение геометрии флага и ускореня, сообщаемого ветром
+    def change_power(self, k):  # Метод описывает изменение геометрии флага и ускорения, сообщаемого ветром
         x_peak = self.x + k * 20
         self.k = k
         self.canvas.coords(self.triangle, self.x, self.y - 160, x_peak, self.y_peak, self.x, self.y - 200)
