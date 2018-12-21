516b948bbd26e74cc28a1017e999ffe7814b093e
AndreyTyshevich <43672780+AndreyTyshevich@users.noreply.github.com>
2018-12-21 12:36:14 +0300

    Update game.py
    последние штрихи

--- game.py
+++ game.py
@@ -3,7 +3,7 @@ from cannon import *
 from flag import *
 
 from tkinter import *
-from random import randrange as randint, choice
+from random import randrange as randint
 from enum import Enum
 
 sleep_time = 35
@@ -42,22 +42,27 @@ class Start_game():
         flag = Flag(canvas, x, y, k)
         self.flag = Flag(canvas, x, y, k)
 
-        x = randint(50, 550)
+        x = randint(100, 550)
         y = self.ground.height[round(x)]
-        self.gamer1 = Cannon(x, y, canvas)
-        self.cannons.append(Cannon(x, y, canvas))
+        self.gamer1 = Cannon(x, y, canvas, root)
+        self.cannons.append(self.gamer1)
 
-        x = randint(750, 1150)
+        x = randint(750, 1100)
         y = self.ground.height[round(x)]
-        self.gamer2 = Cannon(x, y, canvas)
-        self.cannons.append(Cannon(x, y, canvas))
+        self.gamer2 = Cannon(x, y, canvas, root)
+        self.cannons.append(self.gamer2)
 
         self.current_player = 0
+        self.inactive = 1
         self.shells = []
+
+        btn1 = Button(root, text="Armor Up",  width=30, height=5, bg="white", fg="black")
+        btn1.pack()
 
         canvas.bind("<Button-1>", self.mouse_click)
         canvas.bind("<Motion>", self.mouse_motion)
-        # root.bind('<Key>', self.move)
+        btn1.bind("<Button-1>", self.buy_health)
+
         self.game_state = GameState.TANK_IS_AIMING
 
     def mouse_motion(self, event):  # Метод описывает движение мышкой
@@ -65,6 +70,12 @@ class Start_game():
             return
         cannon = self.cannons[self.current_player]
         cannon.target(event.x, screen_height - event.y)
+
+    def buy_health(self):
+        cannon = self.cannons[self.current_player]
+        if cannon.money >= 20:
+            cannon.heal_up()
+            cannon.spend_points()
 
     def mouse_click(self, event):  # Метод описывает клик мышкой
         if self.game_state != GameState.TANK_IS_AIMING:
@@ -78,6 +89,7 @@ class Start_game():
         self.game_state = GameState.SHELL_IS_FLYING
         canvas.after(sleep_time, self.shell_flying)
 
+        self.inactive = self.current_player
         self.current_player = (self.current_player + 1) % 2
 
     def shell_flying(self, *ignore):  # Метод описывает полёт снаряда
@@ -92,10 +104,17 @@ class Start_game():
             if self.ground.check_collision(shell):
                 self.ground.explode(shell)
                 self.game_state = GameState.TANK_IS_AIMING
+            for cannon_object in self.cannons:
+                if self.ground.check_direct_hit(shell, cannon_object):
+                    enemy = self.cannons[self.inactive]
+                    enemy.earn_points()
+                    cannon_object.take_damage(shell.damage)
+                    self.ground.explode(shell)
+                    self.game_state = GameState.TANK_IS_AIMING
         if self.game_state != GameState.SHELL_IS_FLYING:
             self.shells.clear()
 
-            k = randint(-7, 7)
+            k = randint(-10, 10)
             self.flag.change_power(k)
 
 
